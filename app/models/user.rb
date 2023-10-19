d57924b8ec6ea6125df30dfddbbb7a9b0db1a570
sp8783 <p.sp8783@gmail.com>
2023-10-19 21:27:27 +0900

    Add validation checks when setting user icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
   end
+
+  validate :avatar_type
+
+  private
+
+  def avatar_type
+    return unless avatar.attached?
+    unless avatar.image?
+      avatar.purge
+      errors.add(:avatar, I18n.t('errors.messages.avatar_invalid'))
+    end
+  end
 end
