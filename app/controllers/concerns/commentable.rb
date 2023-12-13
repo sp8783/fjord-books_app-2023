2c9b23f0d25c2edd5189a86948e52b8995a52c2a
sp8783 <p.sp8783@gmail.com>
2023-12-14 01:05:31 +0900

    Change `comment` in create action from instance variable to local variable

--- app/controllers/concerns/commentable.rb
+++ app/controllers/concerns/commentable.rb
@@ -4,12 +4,12 @@ module Commentable
   extend ActiveSupport::Concern
 
   def create
-    @comment = @commentable.comments.build(comment_params.merge(user: current_user))
+    comment = @commentable.comments.build(comment_params.merge(user: current_user))
 
-    if @comment.save
-      redirect_to polymorphic_url([@commentable, @comment]), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    if comment.save
+      redirect_to polymorphic_url([@commentable, comment]), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
-      redirect_to polymorphic_url([@commentable, @comment]), status: :unprocessable_entity
+      redirect_to polymorphic_url([@commentable, comment]), status: :unprocessable_entity
     end
   end
 
