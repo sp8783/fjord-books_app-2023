2a06d7d8bbfafa7b880254f62218e798fcb69d9b
sp8783 <p.sp8783@gmail.com>
2024-02-22 04:00:35 +0900

    Change to combine `Report#save_mentions` tests into one test

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -13,16 +13,13 @@ class ReportTest < ActiveSupport::TestCase
     assert_instance_of Date, reports(:one).created_on
   end
 
-  test 'Report#save_mentions should save mentions when new mention to report are saved' do
-    new_mentioning_report = users(:carol).reports.new(title: 'This is a GOOD report!!', content: 'http://localhost:3000/reports/1')
+  test 'Report#save_mentions' do
+    new_mentioning_report = users(:bob).reports.new(title: 'This is a GOOD report!!', content: 'http://localhost:3000/reports/1')
     new_mentioning_report.save
     assert_includes reports(:one).mentioned_reports, new_mentioning_report
-  end
 
-  test 'When an existing mention is deleted by editing, the mention relationship should be also deleted.' do
-    mentioning_report = reports(:two_mentioning_one)
-    mentioning_report.content = 'deleted the mention.'
-    mentioning_report.save
-    assert_not_includes reports(:one).mentioned_reports, mentioning_report
+    new_mentioning_report.content = 'deleted the mention.'
+    new_mentioning_report.save
+    assert_not_includes reports(:one).mentioned_reports, new_mentioning_report
   end
 end
