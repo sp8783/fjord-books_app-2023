a291f8237bcd435da6f1c198c89ee7928980ee33
sp8783 <p.sp8783@gmail.com>
2024-02-22 13:17:37 +0900

    Change to combine `User#name_or_email' tests into one test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'User#name_or_email should return name if user has name' do
+  test 'User#name_or_email' do
     assert_equal 'alice', users(:alice).name_or_email
-  end
 
-  test 'User#name_or_email should return email if user do not has name' do
-    assert_equal 'bob@example.com', users(:bob).name_or_email
+    users(:alice).name = nil
+    assert_equal 'alice@example.com', users(:alice).name_or_email
   end
 end
